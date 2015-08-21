ab2450b640f6088a9e07b33ceacda6b65de62ee3
zycbobby <zuoyuchu@gmail.com>
2015-08-21 12:52:07 +0800

    sign.sh -> release.sh

--- sign.sh
+++ sign.sh
@@ -1,3 +1,13 @@
+#! /bin/zsh
+# from http://ionicframework.com/docs/guide/publishing.html
+
+gulp remove-proxy
+
+if [ -e 'easydownload.apk' ] 
+then 
+    rm easydownload.apk 
+fi
+
 ionic build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $1 ./platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
 ~/Android/Sdk/build-tools/22.0.1/zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk easydownload.apk
