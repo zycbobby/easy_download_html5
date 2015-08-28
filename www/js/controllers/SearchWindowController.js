330fd4e3649b0e1c39fc7d017a85db12eb45f9d4
zycbobby <zuoyuchu@gmail.com>
2015-08-28 16:44:10 +0800

    Remove search function in recent

--- www/js/controllers/SearchWindowController.js
+++ www/js/controllers/SearchWindowController.js
@@ -2,11 +2,6 @@ angular.module('starter.controllers').controller('SearchWindowCtrl', function ($
                                                                          $ionicUser, $ionicPush, $ionicPlatform,
                                                                                $ionicScrollDelegate,
                                                                          $ionicLoading, userService, historyService, articleService, Es) {
-
-        $scope.query = {
-            keyword: ''
-        };
-
         var Window = articleService.Window;
 
         $scope.window  = new Window();
@@ -18,18 +13,10 @@ angular.module('starter.controllers').controller('SearchWindowCtrl', function ($
             });
         }
 
-        $scope.isRecent = true;
         $scope.$on('$ionicView.enter', function(e) {
-            if ($stateParams.keyword) {
-                $scope.query.keyword = $stateParams.keyword;
-                $scope.search($stateParams.keyword);
-            } else {
-                loadingWrapper($scope.window.init().then(function(window){
-                    $scope.window = window;
-                }));
-            }
-
-            Es.listenSearchKeyword(historyService.onSearch);
+            loadingWrapper($scope.window.init().then(function(window){
+                $scope.window = window;
+            }));
         });
 
 
@@ -56,26 +43,6 @@ angular.module('starter.controllers').controller('SearchWindowCtrl', function ($
                 }));
             }
         };
-
-        $scope.search = function (keyword) {
-            $scope.isRecent = false;
-            loadingWrapper(Es.searchThing(keyword).then(function (things) {
-                var articles = [];
-                for (var i = 0; i < things.length; i++) {
-                    var thing = things[i];
-                    var score = thing._score;
-                    if (score > 4) {
-                        articles.push(convertThingToArticle(thing));
-                    }
-                }
-                $scope.isRecent = false;
-                $scope.articles = articles;
-            }));
-        };
-
-        $scope.$on('$ionicView.leave', function(){
-            Es.removeListener(historyService.onSearch);
-        });
 
         function convertThingToArticle(thing) {
             return {
