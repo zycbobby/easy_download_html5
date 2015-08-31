2f9adc2e0266175e4bef03f328d8b1c7fbab1e8a
zycbobby <zuoyuchu@gmail.com>
2015-08-31 17:01:32 +0800

    Finish search

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-    .controller('AppCtrl', function ($scope, $rootScope, $ionicModal, $timeout, $ionicUser, $ionicPush, $ionicPlatform, userService) {
+    .controller('AppCtrl', function ($scope, $rootScope, $state, $ionicFilterBar) {
 
         // With the new view caching in Ionic, Controllers are only called
         // when they are recreated or on app start, instead of every page change.
@@ -8,4 +8,21 @@ angular.module('starter.controllers', [])
         // listen for the $ionicView.enter event:
         //$scope.$on('$ionicView.enter', function(e) {
         //});
+
+
+        $scope.showSearchIcon = $state.is('app.recent');
+
+        $scope.showSearch = function () {
+            $ionicFilterBar.show({
+                asyncSearch: function(keyword){
+                    console.log('search' + keyword);
+                    $state.go('app.search', {keyword: keyword});
+                }
+            });
+        };
+
+        $rootScope.$on('$stateChangeSuccess',
+            function (event, toState, toParams, fromState, fromParams) {
+                $scope.showSearchIcon = $state.is('app.recent');
+            });
     });
